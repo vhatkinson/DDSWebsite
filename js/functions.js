64ed51c2f5be347832cdd9b6dad86848673c8411
Alexir563 <alexir563@aol.com>
2015-07-28 15:29:40 -0400

    Jul 28 3:27PM
    -Dr. DeLucia comercial on Testimonials
    -different layout for Dermal Fillers page
    -Added to header

--- js/functions.js
+++ js/functions.js
@@ -1,49 +1,72 @@
-/*Slide show code
-var slideShowPictures = new Array();
-slideShowPictures[0] = new Image(300, 400);
-slideShowPictures[0].src = "DSC_2273-1.jpg";
-slideShowPictures[1] = new Image(300, 400);
-slideShowPictures[1].src = "DSC_2274-2.jpg";
-slideShowPictures[2] = new Image(300, 400);
-slideShowPictures[2].src = "DSC_2277-3.jpg";
-slideShowPictures[3] = new Image(300, 400);
-slideShowPictures[3].src = "DSC_2282-8.jpg";
-slideShowPictures[4] = new Image(300, 400);
-slideShowPictures[4].src = "DSC_2284-10.jpg";
-slideShowPictures[5] = new Image(300, 400);
-slideShowPictures[5].src = "DSC_2289-15.jpg";
-
-var slide = 0;
-
-function navigateSlides(slideNumber) {
-    slide = slide + slideNumber;
-    if (slide > slideShowPictures.length-1) {
-        slide = 0;
-    }
-    else if (slide = 0) {
-        slide = slideShowPictures.length - 1;
-    }
+/*****Menu Animation*****/
+function showMenu() {
+    document.getElementById("submenu").style.display="block";
 }
-document.getElementById("slideShow").src = slideShowPictures[slide].src;*/
-
-/*Show Menu
-
-var i;
-
-function showmenu(x) {
-    for(i=1; i<=4;i++) {
-        document.getElementById("menu"+i).style.display="none";
-    }
-    if(x>0)
-        document.getElementById("menu"+x).style.display="block";
+function hideMenu() {
+    document.getElementById("submenu").style.display="none";
 }
 
-function showallmenu() {
-    for(i=1;i<=4;i++) {
-        if(document.getElementById("menu"+i).style.display=="block")
-            document.getElementById("menu"+i).style.display="none";
-        else
-            document.getElementById("menu"+i).style.display="block";
+/*****Slide Show*****/
+var imgSlide;
+var pic = 0;
+var images = new Array();
+var timer;
+
+window.onload = function() {
+    imgSlide = document.getElementById("slideShow_img");
+
+    //Pre load images
+    images[0] = new Image();
+    images[0].src = "../images/smile_gallery/DSC_1522-5.jpg";
+    images[1] = new Image();
+    images[1].src = "../images/smile_gallery/Untitled copy.png";
+    images[2] = new Image();
+    images[2].src = "../images/smile_gallery/Untitled.png";
+
+    slide();
+}
+
+function slide() {
+    imgSlide.src = images[pic].src;
+
+    if(pic < images.length-1) {
+        pic++;
+    }
+    else {
+        pic = 0;
+    }
+
+    timer = setTimeout(slide, 5000);
+}
+
+function prev() {
+    if(timer) {
+        stopSlide();
+    }
+    if(pic == 0) {
+        pic = images.length - 1;
+        imgSlide.src = images[pic].src;
+    }
+    else {
+        pic--;
+        imgSlide.src = images[pic].src;
     }
 }
- */
+
+function next() {
+    if(timer) {
+        stopSlide();
+    }
+    if(pic == images.length - 1) {
+        pic = 0;
+        imgSlide.src = images[pic].src;
+    }
+    else {
+        pic++;
+        imgSlide.src = images[pic].src;
+    }
+}
+
+function stopSlide() {
+    clearTimeout(timer);
+}
